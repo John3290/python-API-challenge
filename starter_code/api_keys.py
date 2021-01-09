6a5237eeb98f0884774639ab83b3f348c6e894d1
John3290 <45977820+John3290@users.noreply.github.com>
2021-01-08 19:04:25 -0500

    d
    d

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "a885ffcdacdd0e4029cda18c950070b7"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = " AIzaSyAWEVRahN_3Jcwh6lfEsG4rLOzYcRrFeMw"
